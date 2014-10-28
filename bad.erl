67dfe9aa5d3942dcb82139f5486b905447e1ceaf
dubbl <dubbel14@gmail.com>
2014-10-28 16:15:24 +0100

    BAD: Receiving and answering to BAD pings
    BADs are now acknowledging  received pings and answer with their
    positions. BADs now keep track of other BADs in the area in a list. Not
    implemented: Deleting BADs that are not answering for a prolonged
    timespan.

--- bad.erl
+++ bad.erl
@@ -10,7 +10,7 @@
 % ENV: environment process ID
 start(ENV) ->
 	% spawning new BAD process
-	BAD = spawn(fun() -> bad_loop({undefined, undefined}) end),
+	BAD = spawn(fun() -> bad_loop({undefined, undefined}, dict:new()) end),
 	% setting time to ping
 	{ok,PingTimer} = timer:apply_interval(?PING_INTERVAL, bad, ping, [ENV, BAD]),
 	{BAD,PingTimer}.
@@ -24,14 +24,23 @@ ping(ENV, BAD) ->
 	BAD ! {ENV, time2ping}.
 	
 	
-bad_loop({Lat, Long}) ->
+bad_loop({Lat, Long}, BADList) ->
 	receive
+		% stopping BAD
 		{From, stop} ->
 			io:format("BAD ~p: BAD shutting down~n", [self()]),
 			reply_ok(From);
+		% informing BAD that it is time to ping for new BADs
 		{ENV, time2ping} ->
 			ENV ! {self(), ping, {Lat, Long}},
-			bad_loop({Lat, Long})
+			bad_loop({Lat, Long}, BADList);
+		% receiving ping acknowledgement by other BAD
+		{From, pingACK, {OtherLat, OtherLong}} ->
+			bad_loop({Lat, Long}, dict:store(From, {OtherLat, OtherLong}, BADList));
+		% receiving ping by other BAD
+		{From, ping, {OtherLat, OtherLong}} ->
+			reply(From, {pingACK, {Lat, Long}}),
+			bad_loop({Lat, Long}, dict:store(From, {OtherLat, OtherLong}, BADList))
 	end.
 			
 			
