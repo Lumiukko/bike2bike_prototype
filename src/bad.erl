a6cffed86a655099e6db6d92208bdecd67497f23
dubbl <dubbel14@gmail.com>
2014-10-29 14:15:22 +0100

    BADs communicate accident events
    BADs go in a special accident routine where they try to reach all known
    BADs in range and react to pings to find new BADs that just came into
    range.

--- src/bad.erl
+++ src/bad.erl
@@ -5,6 +5,7 @@
 -import(timer, [apply_interval/4,cancel/1]).
 
 -define(PING_INTERVAL, 5000).
+-define(ACCIDENT_TIMEOUT, 3000).
 
 % starting new BAD
 % ENV: environment process ID
@@ -21,30 +22,57 @@ stop(PID, PingTimer) ->
 	rpc(PID, stop),
 	timer:cancel(PingTimer).
 
-ping(ENV, BAD) ->
-	io:format("Timer: Time to ping! Env: ~p BAD: ~p~n",[ENV, BAD]),
-	BAD ! {ENV, time2ping}.
+ping(ENV, BAD) -> BAD ! {ENV, time2ping}.
 	
 	
 bad_loop(MPA, {Lat, Long}, BADList) ->
+	Self = self(),
 	receive
 		% stopping BAD
 		{From, stop} ->
-			io:format("BAD ~p: BAD shutting down~n", [self()]),
+			io:format("BAD ~p: BAD shutting down~n", [Self]),
 			reply_ok(From);
 		% ENV informing BAD that it is time to ping for new BADs
 		{ENV, time2ping} ->
-			io:format("BAD ~p: Ping broadcast~n", [self()]),
+			io:format("BAD ~p: Ping broadcast~n", [Self]),
 			reply(ENV, {ping, {Lat, Long}}),
 			bad_loop(MPA, {Lat, Long}, BADList);
+		{event, accident} ->
+			case MPA of
+				undefined ->
+					%send msg to other BADs
+					io:format("BAD ~p: Accident recognized. MPA non connected, calling other BADs. ~n", [Self]),
+					{List, _} = dict:to_list(BADList),
+					bad_accident_loop(MPA, {Lat, Long}, BADList, List);
+				MPA ->
+					%send msg to MPA
+					io:format("BAD ~p: Accident recognized. MPA is informed. ~n", [Self]),
+					bad_loop(MPA, {Lat, Long}, BADList)
+			end;
+		{From, event, accident_inform, {Lat, Long}} ->
+			case MPA of
+				undefined ->
+					%send msg to other BADs
+					io:format("BAD ~p: Informed about accident of BAD ~p, not connected to MPA though. ~n", [Self, From]),
+					From ! {self(), event, accident_not_reported},
+					bad_loop(MPA, {Lat, Long}, BADList);
+				MPA ->
+					%send msg to MPA
+					io:format("BAD ~p: Informed about accident of BAD ~p. MPA is informed. ~n", [Self, From]),
+					From ! {self(), event, accident_reported},
+					bad_loop(MPA, {Lat, Long}, BADList)
+			end;
 		% ENV informing BAD that it is time to ping for new BADs
 		{setloc, {NewLat, NewLong}} ->
-			io:format("BAD ~p: I am now at a new position~n", [self()]),
+			io:format("BAD ~p: BAD is at a new position~n", [Self]),
 			bad_loop(MPA, {NewLat, NewLong}, BADList);
 		% receiving ping acknowledgement by other BAD
 		{From, {pingACK, {OtherLat, OtherLong}}} ->
 			io:format("BAD ~p: Received ping ACK from ~p~n", [self(),From]),
 			bad_loop(MPA, {Lat, Long}, dict:store(From, {OtherLat, OtherLong}, BADList));
+		% receiving own ping, disregard it
+		{Self, {ping, _}} ->
+			bad_loop(MPA, {Lat, Long}, BADList);
 		% receiving ping by other BAD
 		{From, {ping, {OtherLat, OtherLong}}} ->
 			io:format("BAD ~p: Received ping from ~p~n", [self(), From]),
@@ -54,30 +82,65 @@ bad_loop(MPA, {Lat, Long}, BADList) ->
 		{From, register} ->
 			case MPA of
 				undefined ->
-					io:format("BAD ~p: BAD paired with MPA ~p~n", [self(), From]),
+					io:format("BAD ~p: BAD paired with MPA ~p~n", [Self, From]),
 					reply_ok(From),
 					bad_loop(From, {Lat, Long}, BADList);
 				_else ->
-					io:format("BAD ~p: Pairing with ~p failed: Already paired with MPA ~p~n", [self(), From,MPA]),
+					io:format("BAD ~p: Pairing with ~p failed: Already paired with MPA ~p~n", [Self, From,MPA]),
 					reply_fail(From)
 			end;
 		% unregistration msg by paired MPA
 		{MPA, unregister} ->
-			io:format("BAD ~p: BAD unpairs from MPA ~p~n", [self(), MPA]),
+			io:format("BAD ~p: BAD unpairs from MPA ~p~n", [Self, MPA]),
 			reply_ok(MPA),
 			bad_loop(undefined, {Lat, Long}, BADList);
 		% unregistration msg by unknown MPA, fail
 		{Other, unregister} ->
-			io:format("BAD ~p: BAD unpairing with MPA ~p failed: Unknown MPA~n", [self(), Other]),
+			io:format("BAD ~p: BAD unpairing with MPA ~p failed: Unknown MPA~n", [Self, Other]),
 			reply_fail(Other),
 			bad_loop(MPA, {Lat, Long}, BADList);
 		{From, Msg} ->
-			io:format("BAD ~p: Received unknown message from ~p: ~p~n", [self(), From, Msg]),
+			io:format("BAD ~p: Received unknown message from ~p: ~p~n", [Self, From, Msg]),
 			bad_loop(MPA, {Lat, Long}, BADList)
 	end.
-			
-			
-			
+
+bad_accident_loop(MPA, {Lat, Long}, BADList, []) ->
+	io:format("BAD ~p: Accident report: None of the known BADs answered/can report the accident, try again~n", [self()]),
+	{List, _} = dict:to_list(BADList),
+	bad_accident_loop(MPA, {Lat, Long}, BADList, List);
+bad_accident_loop(MPA, {Lat, Long}, BADList, [CurrentBAD|List]) ->
+	Self = self(),
+	CurrentBAD ! {Self, event, accident_inform, {Lat, Long}},
+	receive
+		{From, event, accident_reported} ->
+			io:format("BAD ~p: Accident reported by BAD ~p~n", [Self, From]),
+			bad_loop(MPA, {Lat, Long}, BADList);
+		{From, event, accident_not_reported} ->
+			io:format("BAD ~p: Accident report: BAD ~p does not have an MPA either, contacting next known BAD~n", [Self, From]),
+			bad_accident_loop(MPA, {Lat, Long}, BADList, List);
+		{setloc, {NewLat, NewLong}} ->
+			io:format("BAD ~p: BAD is at a new position~n", [Self]),
+			bad_accident_loop(MPA, {NewLat, NewLong}, BADList, List);
+		% receiving ping acknowledgement by other BAD
+		{From, {pingACK, {OtherLat, OtherLong}}} ->
+			io:format("BAD ~p: Received ping ACK from ~p~n", [Self,From]),
+			bad_accident_loop(MPA, {Lat, Long}, dict:store(From, {OtherLat, OtherLong}, BADList), List);
+		% receiving own ping, disregard it
+		{Self, {ping, _}} ->
+			bad_accident_loop(MPA, {Lat, Long}, BADList, List);
+		% receiving ping by other BAD
+		{From, {ping, {OtherLat, OtherLong}}} ->
+			io:format("BAD ~p: Received ping from ~p~n", [Self, From]),
+			reply(From, {pingACK, {Lat, Long}}),
+			bad_accident_loop(MPA, {Lat, Long}, dict:store(From, {OtherLat, OtherLong}, BADList), [List|From])
+		after 
+			?ACCIDENT_TIMEOUT ->
+			io:format("BAD ~p: Accident report: BAD ~p did not answer, contacting next known BAD~n", [Self, CurrentBAD]),
+			bad_accident_loop(MPA, {Lat, Long}, BADList, List)
+	end.
+	
+
+
 % Standard communication functions
 
 rpc(Pid, Request) ->
